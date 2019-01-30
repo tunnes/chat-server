e53d02d84de7d9f1863cdc400773dc428746e8f1
tunnes <ayrton.tunes@outlook.com>
2019-01-30 00:57:12 -0200

    🖤 Unique subscribles on brodcast.

--- app/services/connection/broadcast_service.rb
+++ app/services/connection/broadcast_service.rb
@@ -2,7 +2,7 @@ module Connection
   class BroadcastService
     class << self
       def perform(data, *subscriblers)
-        subscriblers.flatten.map do |subscribler|
+        subscriblers.flatten.uniq.map do |subscribler|
           ActionCable.server.broadcast("SUBSCRIBLER::#{subscribler}", data)
         end
       end
